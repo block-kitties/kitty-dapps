b705076f6c81bea8152d3423cb290e7e7d7f60e7
RACHELS MAC <rachelsmac@RACHELSs-MacBook-Air.local>
2018-05-28 16:03:50 +0100

    add signup form

--- src/App.js
+++ src/App.js
@@ -72,10 +72,7 @@ class App extends Component {
             </a> 
             {(function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()} 
           </Col> 
-
-          {/* <iframe src="http://eepurl.com/dwohTb" /> */}
-                   
-
+           <h3> <a style={{fontFamily : "Montserrat", padding : '10px', color : colors.orange4, fontSize: "36px", fontWeight: "bold"}} href="https://weekendcoders.us15.list-manage.com/subscribe?u=5f993461ddc3fb26d21baa2e0&id=7b26b473da" target="_blank"> Sign up to our mailing list </a> </h3>           
         </Row>
       </div>
     );
